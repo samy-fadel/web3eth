c3e36fade40133f2b8b031857d5f9659602e732d
samy fadel <samyfadel@google.com>
2023-06-28 18:31:28 +0200

    samy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ initializeApp({
 });
 
 const db = getFirestore();
-const web3 = new Web3('http://json-rpc.2mnk2ypckfrt988whmbu8lc8n.blockchainnodeengine.com?key=YOUR_API_KEY');
+const web3 = new Web3('http://json-rpc.2mnk2ypckfrt988whmbu8lc8n.blockchainnodeengine.com?key=AIzaSyCj0UG5tcZCy8vPH9DreznhsKhrs6fBeDo');
 
 async function contractExists(contractAddress) {
   const snapshot = await db.collection('contracts').doc(contractAddress).get();
