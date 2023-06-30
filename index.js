5b48204e1c6000d628eef41e9168ec17efa78b44
samy fadel <samyfadel@google.com>
2023-06-30 20:44:44 +0200

    samy

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const db = getFirestore();
 const web3 = new Web3('http://json-rpc.2mnk2ypckfrt988whmbu8lc8n.blockchainnodeengine.com?key=AIzaSyCj0UG5tcZCy8vPH9DreznhsKhrs6fBeDo');
 
 
-async function getLatestBlockNumber() {
-  try {
-    const blockNumber = await web3.eth.getBlockNumber();
-    console.log('Latest block number:', blockNumber);
-    return blockNumber;
-  } catch (error) {
-    console.error('Error getting latest block number:', error);
-    throw error; // or return a default value: e.g., return 0;
-  }
-}
 
-getLatestBlockNumber();
+
+
+web3.eth.getBlockNumber()
+  .then(blockNumber => {
+    console.log('blockNumber is :', blockNumber);
+  })
+  .catch(error => {
+    console.error('Error retrieving transaction details:', error);
+  });
+
+
