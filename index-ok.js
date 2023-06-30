07c98d64cfa2da6cc0f6c03eae45aaaa9b17b52f
samy fadel <samyfadel@google.com>
2023-06-30 20:32:30 +0200

    samy

--- index-ok.js
+++ index-ok.js
@@ -3,7 +3,7 @@ const Web3 = require('web3');
 
 
 // Connect to an Ethereum node
-const web3 = new Web3('http://json-rpc.2mnk2ypckfrt988whmbu8lc8n.blockchainnodeengine.com?key=AIzaSyCj0UG5tcZCy8vPH9DreznhsKhrs6fBeDo');
+const web3 = new Web3('json-rpc.2mnk2ypckfrt988whmbu8lc8n.blockchainnodeengine.com?key=AIzaSyCj0UG5tcZCy8vPH9DreznhsKhrs6fBeDo');
 
 web3.eth.getBlock('latest')
   .then(transaction => {
